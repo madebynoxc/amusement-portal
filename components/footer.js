ec0ce1b1db55110b65785182d2eea2a7b3fe5417
NoxCaos <glebx95@gmail.com>
2019-01-17 20:43:06 -0500

    Removed dbl links

--- components/footer.js
+++ components/footer.js
@@ -11,9 +11,6 @@ export default class Footer extends React.Component {
                     <div className="block">
                         <img src="https://amusementclub.nyc3.digitaloceanspaces.com/web/amusement-logo-small.png"/>
                         <img src="https://amusementclub.nyc3.digitaloceanspaces.com/web/discord.png"/>
-                        <a href="https://discordbots.org/bot/340988108222758934" >
-                          <img src="https://discordbots.org/api/widget/upvotes/340988108222758934.svg" alt="Amusement Club" />
-                        </a>
                         <span>with ❤️ by NoxCaos</span>
                     </div>
                     <div className="block">
@@ -29,7 +26,6 @@ export default class Footer extends React.Component {
                         <ul>
                             <li><a href="https://github.com/NoxCaos/amusement-club">GitHub</a></li>
                             <li><a href="https://ko-fi.com/noxcaos">Donate</a></li>
-                            <li><a href="https://discordbots.org/bot/340988108222758934">Discord bot list</a></li>
                         </ul>
                     </div>
                     <div className="block">
