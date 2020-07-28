6c6c538082ff734b401f306c3c80878ef6d3a76e
NoxCaos <glebx95@gmail.com>
2020-07-28 12:24:43 -0400

    Added OG

--- components/layout.js
+++ components/layout.js
@@ -7,7 +7,17 @@ const Layout = props => (
   <>
     <Head>
       <title>Amusement Club Gacha</title>
+      <meta charset="utf-8" />
       <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width"/>
+      <meta name="description" content="Amusement Club is a global Discord bot for collecting amazing cards from your favourite show or game! Gift a fun game to your Discord community!" />
+      <meta property="og:title" content="Amusement Club: Discord gacha" />
+      <meta property="og:type" content="discord bot" />
+      <meta property="og:url" content="https://amusementclub.xyz" />
+      <meta property="og:image" content="https://amusementclub.nyc3.digitaloceanspaces.com/web/amusement-logo-small.png" />
+      <meta property="og:description" content="Global gacha game for Discord with thousands of cute and fancy cards made by the bot's community." />
+
+      <link rel="shortcut icon" href="/_next/static/favicon.ico" type="image/x-icon" />
+      <link rel="icon" href="/_next/static/favicon.ico" type="image/x-icon" />
     </Head>
     <style jsx global>{`
       *,
